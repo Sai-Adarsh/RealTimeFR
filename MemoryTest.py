c26f2c6c1cd141ea3c0df1e7e8f3c4f9c6cdae89
Sai Adarsh <sivakumar.sk11865.sk@gmail.com>
2018-06-26 13:24:59 +0530

    Initial commit

---
+++ MemoryTest.py
@@ -0,0 +1,4 @@
+print("Loading...")
+import RealTimeMTModel
+model = RealTimeMTModel.get_model('C:\\Users\\saiadarsh\\Desktop\\RealTimeFR-master\\model_resnet_cifar10-64000')
+print("Done loading...")
